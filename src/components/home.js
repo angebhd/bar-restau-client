b756fdd41ee3084f0ad768ee4bb950b4b9ff33d4
Asifiwe BUHENDWA <mickaelbhd@gmail.com>
2023-09-13 16:31:51 +0300

    component update

--- src/components/home.js
+++ src/components/home.js
@@ -1,3 +1,4 @@
+import Hmenu from "./hmenu";
 import NavItems from "./navitems";
 
 function Home (){
@@ -7,6 +8,10 @@ function Home (){
                 <h1>Bar-restau</h1>
                 <NavItems></NavItems>
             </div>
+            <div>
+                <p>The best flavour of the country</p>
+            </div>
+            <Hmenu></Hmenu>
         </div>
     )
 }
