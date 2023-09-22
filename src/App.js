8293584e5a20676b5bbec878639f6bd93dd92703
Asifiwe BUHENDWA <mickaelbhd@gmail.com>
2023-09-22 18:09:03 +0200

    begin with backend-change repository name

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Home from './components/home'
+import Menu from './components/menu';
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <Menu/>
     </div>
   );
 }
