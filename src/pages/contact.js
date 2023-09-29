f93947b69c18b4b4102b41da644e6e937d3c9938
Asifiwe BUHENDWA <mickaelbhd@gmail.com>
2023-09-29 21:05:23 +0200

    full client part without design

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,9 +2,10 @@ import SocialNetwork from "../components/contact/socialnetwork"
 
 function Contact () {
     return(
+        <>
         <h1>Contact</h1>
         <SocialNetwork/>
-
+        </>
     )
 }
 
--- src/pages/reservation.js
+++ src/pages/reservation.js
@@ -2,21 +2,22 @@ function Reservation() {
     return(
         <>
             <h2>Reservation</h2>
-            <form>
+            <form method="post" action="">
                 <span>Choose the type of table : </span>
-                <select name="Table">
-                    <option value={"one"}>Table for one</option>
-                    <option value={"two"}>Table for two</option>
-                    <option value={"four"}>Table for four</option>
-                    <option value={"eigt"}>Table for eight</option>
+                <select name="table">
+                    <option value={1}>Table for one</option>
+                    <option value={2}>Table for two</option>
+                    <option value={4}>Table for four</option>
+                    <option value={8}>Table for eight</option>
                 </select>
     
                 <br></br>
                 <span>Time : </span>
-                <input type="time"></input>
+                <input name='time' type="time"></input>
                 <br></br>
                 <span>Name : </span>
-                <input placeholder="Ange BHD"></input>
+                <input placeholder="Ange BHD" name="name"></input>
+                <input type="submit"></input>
             </form>
         </>
 
